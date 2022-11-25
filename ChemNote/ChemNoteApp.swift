b29b7d9327950b24d2ad2006253e1bebfe72ad29
yutteee <yusaku_nakamura_aa@icloud.com>
2022-11-25 19:41:14 +0900

    Initial Commit

---
+++ ChemNote/ChemNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChemNoteApp.swift
+//  ChemNote
+//
+//  Created by nakamura yusaku on 2022/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ChemNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
